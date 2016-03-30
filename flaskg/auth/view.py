0aa62e608e3876a9aeacdf6d7b81d79f173dc11d
gongxj <gongxiujin@jingtum.com>
2016-03-30 10:05:28 +0800

    change

--- flaskg/auth/view.py
+++ flaskg/auth/view.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 from flaskg.auth.form import NameForm
+from flaskg.auth.models import User
 
 auth = Blueprint('auth', __name__)
 
@@ -8,4 +9,6 @@ def index(name):
     name_form = NameForm()
     if name_form.validate_on_submit():
         name = name_form.name.data
+        user = User(name)
+        user.save()
     return render_template('auth/index.html', name=name, form=name_form)
